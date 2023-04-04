d89db78f6b53794469c15b5c035f8ab1041064c1
Denis Bucspun <bucspun.d@gmail.com>
2023-04-04 08:56:22 +0200

    :construction: in progress

--- script.js
+++ script.js
@@ -12,4 +12,41 @@
         // Redirigez vers une autre page ou lancez la prochaine animation/interaction
         window.location.href = "chemin_vers_votre_prochaine_page.html";
     }})
+
+  // Ajoutez ceci après la déclaration de votre timeline GSAP
+  const customCursor = document.getElementById("baguette");
+
+  function moveCursor(e) {
+      gsap.to(customCursor, {
+          duration: 0.3,
+          x: e.clientX,
+          y: e.clientY,
+          ease: "power2.out"
+      });
+  }
+
+  function showCursor() {
+      gsap.to(customCursor, {
+          duration: 0.3,
+          opacity: 1,
+          ease: "power2.out"
+      });
+  }
+
+  function hideCursor() {
+      gsap.to(customCursor, {
+          duration: 0.3,
+          opacity: 0,
+          ease: "power2.out"
+      });
+  }
+
+  // Événements pour gérer l'apparition, la disparition et le mouvement du curseur personnalisé
+  document.addEventListener("mousemove", moveCursor);
+  document.addEventListener("mouseenter", showCursor);
+  document.addEventListener("mouseleave", hideCursor);
+
+  // Cachez le curseur par défaut
+  document.body.style.cursor = "none";
+
     
